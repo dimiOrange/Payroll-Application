066477f38bc0485c8805582f21a7811f26fa0198
dimiOrange <demeter.samarov@gmail.com>
2019-01-07 15:35:56 +0200

    Added: EmployeeType Form, Regiser Form View, registerAction

--- src/AppBundle/Controller/EmployeeController.php
+++ src/AppBundle/Controller/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Employee;
+use AppBundle\Form\EmployeeType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +11,56 @@ use Symfony\Component\Routing\Annotation\Route;
 class EmployeeController extends Controller
 {
     /**
+     * @Route("/register", name="employee_register")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function registerAction(Request $request)
+    {
+        $employee = new Employee();
+        $form = $this->createForm(EmployeeType::class, $employee);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $emailForm = $form->getData()->getEmail();
+            $employeeForm = $this
+                ->getDoctrine()
+                ->getRepository(Employee::class)
+                ->findOneBy(['email' => $emailForm]);
+
+            if(null !== $employeeForm){
+                $this->addFlash('info', "Username with email " . $emailForm . " already taken!");
+                return $this->render('employee/register.html.twig', ['form' => $form->createView()]);
+            }
+
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($employee, $employee->getPassword());
+
+//            $role = $this
+//                ->getDoctrine()
+//                ->getRepository(Role::class)
+//                ->findOneBy(['name' => 'ROLE_USER']);
+//
+//            $user->addRole($role);
+
+            $employee->setPassword($password);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($employee);
+            $em->flush();
+
+            $this->addFlash('info', "Employee successfully registered");
+            return $this->redirectToRoute("security_login");
+        }
+
+        return $this->render('employee/register.html.twig',
+            ['form' => $form->createView()]);
+    }
+
+    /**
      * @Route("/profile", name="employee_profile")
      */
-    public function profile(){
+    public function profileAction(){
         $employeeId = $this
             ->getUser()
             ->getId();
---
+++ src/AppBundle/Form/EmployeeType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('fullName', TextType::class)
+            ->add('companyId', TextType::class)
+            ->add('bankAccount', TextType::class)
+            ->add('baseSalary', NumberType::class)
+            ->add('email', TextType::class)
+            ->add('password', RepeatedType::class,
+                [
+                    'type' => PasswordType::class,
+                    'first_options'  => array('label' => 'Password'),
+                    'second_options' => array('label' => 'Repeat Password'),
+                ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Employee'
+        ));
+    }
+}
