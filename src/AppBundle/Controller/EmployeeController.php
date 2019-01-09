5fe464e8ce58849524e98300c2ff8540e6808096
dimiOrange <demeter.samarov@gmail.com>
2019-01-09 10:25:47 +0200

    Created: ChangePassword Controler and View

--- src/AppBundle/Controller/EmployeeController.php
+++ src/AppBundle/Controller/EmployeeController.php
@@ -6,6 +6,8 @@ use AppBundle\Entity\Role;
 use AppBundle\Entity\Employee;
 use AppBundle\Form\EmployeeType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,7 +21,7 @@ class EmployeeController extends Controller
     public function registerAction(Request $request)
     {
         $currEmployee = $this->getUser();
-        if ($currEmployee === null || !$currEmployee->isPersonnelManager() ) {
+        if ($currEmployee === null || !$currEmployee->isPersonnelManager()) {
             return $this->redirectToRoute('homepage');
         }
 
@@ -27,14 +29,14 @@ class EmployeeController extends Controller
         $form = $this->createForm(EmployeeType::class, $employee);
         $form->handleRequest($request);
 
-        if($form->isSubmitted()){
+        if ($form->isSubmitted()) {
             $emailForm = $form->getData()->getEmail();
             $employeeForm = $this
                 ->getDoctrine()
                 ->getRepository(Employee::class)
                 ->findOneBy(['email' => $emailForm]);
 
-            if(null !== $employeeForm){
+            if (null !== $employeeForm) {
                 $this->addFlash('info', "Username with email " . $emailForm . " already taken!");
                 return $this->render('employee/register.html.twig', ['form' => $form->createView()]);
             }
@@ -66,7 +68,8 @@ class EmployeeController extends Controller
     /**
      * @Route("/profile", name="employee_profile")
      */
-    public function profileAction(){
+    public function profileAction()
+    {
         $employeeId = $this
             ->getUser()
             ->getId();
@@ -78,5 +81,57 @@ class EmployeeController extends Controller
 
         return $this->render("employee/profile.html.twig",
             ['employee' => $employee]);
+    }
+
+    /**
+     * @Route("/changePassword", name="employee_change_password")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function changePasswordAction(Request $request)
+    {
+        $employeeId = $this
+            ->getUser()
+            ->getId();
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $employee = $entityManager
+            ->getRepository(Employee::class)
+            ->find($employeeId);
+
+        $defaultData = array('pass' => 'New Password', 'repeatPass' => 'Repeat New Password');
+        $form = $this->createFormBuilder($defaultData)
+            ->add('pass', TextType::class, array('label' => 'New Password'))
+            ->add('repeatPass', TextType::class, array('label' => 'Repeat New Password'))
+            ->add('send', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $pass = $data['pass'];
+            $repeatPass = $data['repeatPass'];
+
+            if ($pass !== $repeatPass) {
+                return $this->render('employee/changePassword.html.twig', array(
+                    'form' => $form->createView(),
+                ));
+            }
+
+            $employee->setPassword($pass);
+
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($employee, $employee->getPassword());
+
+            $employee->setPassword($password);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('employee/changePassword.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
